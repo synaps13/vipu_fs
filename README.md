596bf6989637adf692b4c85437b5ea2b96513011
Rahimgulov Rustam <rahimgulov.rustam@gmail.com>
2025-05-25 17:17:34 +0300

    progress

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Refernce design
+
+https://nicepage.com/landing-page/preview/website-design-firm-2625087?device=desktop
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,88 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { FaComputer, FaTruck, FaPlug, FaHandHoldingHeart } from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<>
+			{Hero()}
+			{Testimonials()}
+			{About()}
+		</>
+	);
+}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+function Hero() {
+	return (
+		<section>
+			<div className={styles.heroRow}>
+				<div className={styles.heroTextBlock}>
+					<h6>Who we are?</h6>
+					<h1>We Set Up Your Tech, <span>So You Don’t Have To</span></h1>
+					<p>Laptops, printers, Wi-Fi, servers, anything in between — we’ll get everything working smoothly while you focus on your team.</p>
+					<a className={styles.button} href="#">Let's chat about your IT problem</a>
+				</div>
+
+				<div className={styles.heroImgBlock}>
+				</div>
+			</div>
+
+			<div className={styles.serviceIcons}>
+				<div className={styles.serviceIconBlock}>
+					<FaComputer />
+					<p>Complete device setup</p>
+				</div>
+
+				<div className={styles.serviceIconBlock}>
+					<FaTruck />
+					<p>Help with office relocation</p>
+				</div>
+
+				<div className={styles.serviceIconBlock}>
+					<FaPlug />
+					<p>Peripheral configuration</p>
+				</div>
+
+				<div className={styles.serviceIconBlock}>
+					<FaHandHoldingHeart />
+					<p>Easy to understand process</p>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+function Testimonials() {
+	return (
+		<section className={styles.testimonials}>
+			<h2>What clients say</h2>
+			<p>We place huge value on strong relationsps and have seen the benefit they bring to the businesses. Customer feedback is vital in helping us to get it right.</p>
+
+			<div className={styles.testimonialsBlocks}>
+				<div className={styles.testimonialCard}>
+					<p>“We moved to a new location and needed everything—network, server, and devices—reinstalled. These guys handled it all smoothly over the weekend. Highly recommended!”</p>
+					<h4>Alex G., Office Manager</h4>
+				</div>
+				<div className={styles.testimonialCard}>
+					<p>“As a startup, we needed a quick and reliable tech setup without hiring in-house IT. This service was a lifesaver—easy communication, great support, and everything just worked.”</p>
+					<h4>Liis S., Co-Founder</h4>
+				</div>
+				<div className={styles.testimonialCard}>
+					<p>“We’re a small design studio with no in-house IT. When our office moved, they handled the tech side—computers, Wi-Fi, and backup drives—quickly and without hassle.”</p>
+					<h4>Jaanus K., Manager</h4>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+function About()
+{
+	return (
+		<section>
+			<h2><span>01</span> About Our Studio</h2>
+			<div>
+				<p>We’re a versatile IT partner for businesses of all sizes—offering everything from hands-on tech setup and office relocations to consulting, software development, testing, and recruiting. Whether you need your devices up and running or expert support to grow your team and systems, we’re here to help you move forward with confidence.</p>
+			</div>
+		</section>
+	);
 }

587434a5481d48f42296ec1a1ad699f3106a97f8
Rahimgulov Rustam <rahimgulov.rustam@gmail.com>
2025-08-28 00:27:01 +0300

    switching to bulma

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Refernce design
 
-- https://nicepage.com/landing-page/preview/website-design-firm-2625087?device=desktop
+- https://themefisher.com/demo?theme=orbitor-bulma
 - https://react-icons.github.io/react-icons/icons/fa6/
 
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,19 @@ export default function Home() {
 
 function Hero() {
 	return (
-		<section>
-			<div className={styles.heroRow}>
-				<div className={styles.heroTextBlock}>
-					<h6>Who we are?</h6>
-					<h1>We Set Up Your Tech, <span>So You Don’t Have To</span></h1>
-					<p>Laptops, printers, Wi-Fi, servers, anything in between — we’ll get everything working smoothly while you focus on your team.</p>
-					<a className={styles.button} href="#contact">Let's chat about your IT problem</a>
-				</div>
-
-				<div className={styles.heroImgBlock}>
-				</div>
+		<section className={`hero is-medium ${styles.hero}`}>
+			<div className="hero-body">
+				<p className="title">We Set Up Your Tech, <span>So You Don’t Have To</span></p>
+				<p className="subtitle">Laptops, printers, Wi-Fi, servers, anything in between — we’ll get everything working smoothly while you focus on your team.</p>
+				<a className="button is-primary is-light is-large" href="#contact">Let's chat about your IT problem</a>
 			</div>
+		</section>
+	);
+}
 
+function Services() {
+	return (
+		<section>
 			<div className={`${styles.serviceIcons} ${styles.sectionSpacing}`}>
 				<div className={styles.serviceIconBlock}>
 					<FaComputer />
@@ -61,22 +61,47 @@ function Hero() {
 
 function Testimonials() {
 	return (
-		<section className={`${styles.testimonials} ${styles.sectionSpacing}`}>
-			<h2>What clients say</h2>
-			<p>We place huge value on strong relationsps and have seen the benefit they bring to the businesses. Customer feedback is vital in helping us to get it right.</p>
+		<section className="section">
+			<div className="container">
+				<div className="columns is-desktop">
+					<div className="column is-8-desktop">
+						<p className="title">What they say about us</p>
+						<p className="subtitle">We place huge value on strong relationsps and have seen the benefit they bring to the businesses. Customer feedback is vital in helping us to get it right.</p>
+					</div>
+				</div>
 
-			<div className={styles.testimonialsBlocks}>
-				<div className={styles.testimonialCard}>
-					<p>“We moved to a new location and needed everything—network, server, and devices—reinstalled. These guys handled it all smoothly over the weekend. Highly recommended!”</p>
-					<h4>Alex G., Office Manager</h4>
-				</div>
-				<div className={styles.testimonialCard}>
-					<p>“As a startup, we needed a quick and reliable tech setup without hiring in-house IT. This service was a lifesaver—easy communication, great support, and everything just worked.”</p>
-					<h4>Liis S., Co-Founder</h4>
-				</div>
-				<div className={styles.testimonialCard}>
-					<p>“We’re a small design studio with no in-house IT. When our office moved, they handled the tech side—computers, Wi-Fi, and backup drives—quickly and without hassle.”</p>
-					<h4>Jaanus K., Manager</h4>
+				<div className="container">
+					<div className="columns">
+						<div className="column is-4">
+							<div className="box">
+								<blockquote>We moved to a new location and needed everything—network, server, and devices—reinstalled. These guys handled it all smoothly over the weekend. Highly recommended!</blockquote>
+								<figure className="image is-64x64">
+									<img className="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
+								</figure>
+								<h4>Alex G., Office Manager</h4>
+							</div>
+						</div>
+
+						<div className="column is-4">
+							<div className="box">
+								<blockquote>As a startup, we needed a quick and reliable tech setup without hiring in-house IT. This service was a lifesaver—easy communication, great support, and everything just worked.</blockquote>
+								<figure className="image is-64x64">
+									<img className="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
+								</figure>
+								<h4>Liis S., Co-Founder</h4>
+							</div>
+						</div>
+
+						<div className="column is-4">
+							<div className="box">
+								<blockquote>We’re a small design studio with no in-house IT. When our office moved, they handled the tech side—computers, Wi-Fi, and backup drives—quickly and without hassle.</blockquote>
+								<figure className="image is-64x64">
+									<img className="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
+								</figure>
+								<h4>Jaanus K., Manager</h4>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -145,18 +170,32 @@ function Planning() {
 
 function Contact() {
 	return (
-		<section className={`${styles.sectionSpacing} ${styles.contactSection}`} id="contact">
-			<div className={`${styles.sectionBody} ${styles.contactSectionBody}`}>
-				<h2 className={styles.sectionHeader}>Looking to collaborate? <span>Get in touch to find out how we can help</span></h2>
-				<div className={styles.contactFormBlock}>
-					<h3>Send us message with the form below</h3>
-					<p>We're here to help. Tell us a little about your project and we'll be in touch.</p>
-					<form action={sendEmail}>
-						<input type="text" id="name" name="name" placeholder="Enter your name here"/>
-						<input type="email" id="email" name="email" placeholder="Enter your email here"/>
-						<textarea name="message" id="message" cols={30} rows={10} placeholder="Write a bit about your problem here"></textarea>
-						<button type="submit">Send</button>
-					</form>
+		<section className="section" id="contact">
+			<div className="container">
+				<h2 className="title">Looking to collaborate?</h2>
+				<div className="columns">
+					<div className="column">
+						<h3>Send us message with the form below</h3>
+						<p>We're here to help. Tell us a little about your project and we'll be in touch.</p>
+					</div>
+
+					<div className="column">
+						<form action={sendEmail}>
+							<div className="field">
+								<label className="label">Name</label>
+								<input className="input" type="text" id="name" name="name" placeholder="Enter your name here" />
+							</div>
+							<div className="field">
+								<label className="label">Email</label>
+								<input className="input" type="email" id="email" name="email" placeholder="Enter your email here" />
+							</div>
+							<div className="field">
+								<label className="label">Message</label>
+								<textarea className="input"  name="message" id="message" rows={10} placeholder="Write a bit about your problem here"></textarea>
+							</div>
+							<button className="button is-primary" type="submit">Send</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -165,10 +204,60 @@ function Contact() {
 
 function Footer() {
 	return (
-		<footer className={styles.footer}>
-			<div>Needsleep OU</div>
-			<div>2025</div>
-			<div>another piece of info</div>
+		<footer className="footer">
+			<div className="container">
+				<div className="columns is-multiline">
+					<div className="column is-3-widescreen is-6-tablet">
+						<h3 className="is-size-4 mb-4">NeedSleep</h3>
+						<p>Tempora dolorem voluptatum nam vero assumenda voluptate, facilis ad eos obcaecati tenetur veritatis eveniet distinctio.</p>
+					</div>
+
+					<div className="column is-2-widescreen is-6-desktop is-6-tablet ml-auto">
+						<h4 className="is-capitalized mb-4">Company</h4>
+
+						<ul className="list-unstyled footer-menu lh-35">
+							<li><a href="#">About</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Team</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+
+					<div className="column is-3-widescreen is-6-desktop is-6-tablet">
+						<h4 className="is-capitalized mb-4">Support</h4>
+
+						<ul className="list-unstyled footer-menu lh-35">
+							<li><a href="#">Terms &amp; Conditions</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+							<li><a href="#">Support</a></li>
+							<li><a href="#">FAQ</a></li>
+						</ul>
+					</div>
+
+					<div className="column is-3-widescreen is-6-desktop is-6-tablet">
+						<h4 className="is-capitalized mb-4">Get in Touch</h4>
+						<h6><a href="tel:+23-345-67890"> <i className="ti-headphone-alt mr-3"></i>Support@megakit.com</a></h6>
+						<h6><a href="mailto:support@gmail.com"> <i className="ti-mobile mr-3"></i>+23-456-6588</a></h6>
+
+						<ul className="list-inline footer-socials mt-5">
+							<li className="list-inline-item"><a href="https://www.facebook.com/themefisher"><i className="ti-facebook mr-2"></i></a></li>
+							<li className="list-inline-item"><a href="https://twitter.com/themefisher"><i className="ti-twitter mr-2"></i></a></li>
+							<li className="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i className="ti-linkedin mr-2 "></i></a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div className="footer-btm py-4 mt-6">
+					<div className="columns">
+						<div className="column is-6-widescreen">
+							<div className="copyright">
+								© Copyright Reserved to <span className="text-color">Orbitor</span> by <a href="https://themefisher.com/" target="_blank">Themefisher</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 		</footer>
 	);
 }
